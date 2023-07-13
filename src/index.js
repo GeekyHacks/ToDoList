a6efb5e33a8b6ba8ad50a2f642c3633987f34c10
GeekyHacks <geekyhacks22@gmail.com>
2023-07-13 21:30:21 +0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import './styles/sass/main.sass';
 import refresh from './assets/refresh.png';
-import dots from './assets/three-dots.png';
+// import dots from './assets/three-dots.png';
 import enter from './assets/enter.png';
-import trashCan from './assets/trash-can.png';
+// import trashCan from './assets/trash-can.png';
 // import DisplayTasks from './displayList.js';
-
 const refreshImg = refresh;
-
-const dotsImg = dots;
-
-const trashCanImg = trashCan;
-
+// const dotsImg = dots;
+// const trashCanImg = trashCan;
 // const taskList = new DisplayTasks();
-
 const enterImg = enter;
-export { refreshImg, dotsImg, enterImg, trashCanImg };
+export { refreshImg, enterImg };
